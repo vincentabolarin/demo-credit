bde064651e1f4fd99402be14f04835146162ecad
Vincent Abolarin <80973232+vincentabolarin@users.noreply.github.com>
2025-10-19 01:31:41 +0100

    test pre-push hook

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -15,8 +15,10 @@ describe('AppController', () => {
   });
 
   describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Hello World!');
+    it('should return "Hello World! This is the Demo Credit Application."', () => {
+      expect(appController.getHello()).toBe(
+        'Hello World! This is the Demo Credit Application.',
+      );
     });
   });
 });
